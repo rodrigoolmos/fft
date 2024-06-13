335f8311fb1547ee1e626f1576aaf6dd05424ea8
rodrigo olmos <tic.rodrigo.olmos@gmail.com>
2024-06-13 16:29:39 +0200

    bug signo arreglado

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 #include <string.h>
 
 #define PI 3.14159265358979323846
-#define DIM 1024*64
+#define DIM 1024
+#define EPSILON 0.2
 
 typedef struct {
     float real;
@@ -17,23 +18,23 @@ void FFT(Complex *x, int N) {
     float angle;
     Complex temp, wlen, w, u, v, next_w;
 
+    // Reordenamiento por bit reversal
     j = 0;
-    for (i = 0; i < N; ++i) { // bit reversal
+    for (i = 0; i < N; ++i) {
         if (i < j) {
             temp = x[i];
             x[i] = x[j];
             x[j] = temp;
         }
-        m = N >> 1;
-        while (m >= 1 && j >= m) {
+        for (m = N >> 1; m >= 1 && j >= m; m >>= 1) {
             j -= m;
-            m >>= 1;
         }
         j += m;
     }
 
+    // Algoritmo de la mariposa
     for (len = 2; len <= N; len <<= 1) {
-        angle = -2 * PI / len;
+        angle = 2 * PI / len;
         wlen.real = cos(angle);
         wlen.imag = sin(angle);
         for (i = 0; i < N; i += len) {
@@ -55,60 +56,68 @@ void FFT(Complex *x, int N) {
     }
 }
 
-
 void DFT(Complex *input, Complex *output, int N) {
-    for (int k = 0; k < N; k++) {  // Para cada punto de salida
+    for (int k = 0; k < N; k++) {
         output[k].real = 0;
         output[k].imag = 0;
-        for (int n = 0; n < N; n++) {  // Para cada punto de entrada
-            float angle = 2 * PI * k * n / N;  // Cambio de signo en el ángulo
+        for (int n = 0; n < N; n++) {
+            float angle = 2 * PI * k * n / N;
             output[k].real += input[n].real * cos(angle) - input[n].imag * sin(angle);
             output[k].imag += input[n].imag * cos(angle) + input[n].real * sin(angle);
         }
     }
 }
 
-void printComplexArray(Complex *array, int N) {
-    for (int i = 0; i < N; i++) {
-        printf("(%f, %f)\n", array[i].real, array[i].imag);
-    }
-}
-
 int main() {
     int N = DIM;  // Número de puntos
+    int error = 0;
+    double real_des, real_max;
+    double imag_des, imag_max;
 
-    // Array de entrada de ejemplo (4 puntos)
+    // Array de entrada de ejemplo (DIM puntos)
     Complex input[DIM];
-
     for (int i = 0; i < N; i++) {
-        input[i].real = (float)rand();
-        input[i].imag = (float)rand();
+        input[i].real = (float)rand() * 0.456;
+        input[i].imag = (float)rand() * 0.345;
     }
 
-    // Array de salida
-    Complex output[N];
+    Complex outputDFT[DIM];
+    Complex outputFFT[DIM];
 
-    // Calcular la DFT
+    // Copia de entrada para FFT
+    memcpy(outputFFT, input, sizeof(Complex) * DIM);
+
     clock_t start = clock();
-    DFT(input, output, N);
+    DFT(input, outputDFT, N);
     clock_t end = clock();
-
-    printf("Tiempo de ejecución: %f segundos\n", (double) (end - start) / CLOCKS_PER_SEC);
+    printf("Tiempo de ejecución de la DFT: %f segundos\n", (double) (end - start) / CLOCKS_PER_SEC);
 
     start = clock();
-    FFT(input, N);
+    FFT(outputFFT, N);  // La función FFT modifica outputFFT en su lugar
     end = clock();
+    printf("Tiempo de ejecución de la FFT: %f segundos\n", (double) (end - start) / CLOCKS_PER_SEC);
 
-    printf("Tiempo de ejecución: %f segundos\n", (double) (end - start) / CLOCKS_PER_SEC);
+    real_max = 0;
+    imag_max = 0;
+    for (int i = 0; i < N; i++) {
+        real_des = fabs((outputFFT[i].real - outputDFT[i].real) / outputDFT[i].real);
+        imag_des = fabs((outputFFT[i].imag - outputDFT[i].imag) / outputDFT[i].imag);
+        if(real_des > EPSILON || imag_des > EPSILON)
+            error = 1;
 
-
-    if(memcmp(input, output, sizeof(Complex)* N)){
-        printf("OK\n");
-    }else{
-        printf("ERR\n");
+        if(real_max < real_des)
+            real_max = real_des;
+        if (imag_max < imag_des)
+            imag_max = imag_des;        
     }
 
-    // Imprimir la DFT
+    if(error){
+        printf("ERR\n");
+    } else {
+        printf("OK\n");
+    }
+
+    printf("MAX DIF REAL %f MAX DIF IMAG %f\n", real_max, imag_max);
 
     return 0;
 }
